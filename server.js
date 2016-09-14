4b8e135784433d3620b7d82d8f23caa43ac2c76e
revathimattaparthi <srevathi036@gmail.com>
2016-09-14 11:17:58 +0530

    Update server.js
    updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(middleware.logger);
 //about
 app.get('/about', middleware.requireAuthentication, function(req,res){
 
-	res.send('It is About page !!!!!');
+	res.send('It is About pageeeeeeeeee !!!!!');
 });
 
 
