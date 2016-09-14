e5cc1c9726996a182e1bcf64ead55a6b2343f288
Srevathi <Srevathi036@gmail.com>
2016-09-14 12:48:08 +0530

    server2

---
+++ server2.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+//about
+app.get('/about', middleware.requireAuthentication, function(req,res){
+
+	res.send('It is Aboutttttttt pageeeeeeeeee !!!!!');
+});
+
+
+app.get('/aboutus', middleware.requireAuthentication, function(req, res){
+	res.send('It is Aboutttttttt us pageeeeeeeeee !!!!!');
+
+});
+app.use(express.static(__dirname + '/html'));
+
+app.listen(PORT, function(){
+
+	console.log(' server started ' + PORT + '!');
+});
+
